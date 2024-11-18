b5d69803f6349c28869ff6b83e306b1e2df0bca6
Mohammad Hakimi <hakimi.mohammad.n@gmail.com>
2024-11-18 18:09:26 +0100

    add transformer - add req.txt

--- scrapy_test/scrapy_test/spiders/my_spider.py
+++ scrapy_test/scrapy_test/spiders/my_spider.py
@@ -1,11 +1,17 @@
-import scrapy
 from urllib.parse import urljoin
+
+import scrapy
+from langchain_community.document_transformers import BeautifulSoupTransformer
+from langchain_core.documents import Document
+from scrapy.http import Response
+
 
 class WebCrawler(scrapy.Spider):
     name = "webcrawler"
     start_urls = ["https://www.arol.com/arol-canelli"]
+    transformer: BeautifulSoupTransformer
     custom_settings = {
-        "DEPTH_LIMIT": 1,
+        "DEPTH_LIMIT": 5,
         "ROBOTSTXT_OBEY": True,
         "USER_AGENT": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
         "DOWNLOAD_DELAY": 2,
@@ -23,16 +29,22 @@ class WebCrawler(scrapy.Spider):
     def __init__(self, *args, **kwargs):
         super(WebCrawler, self).__init__(*args, **kwargs)
         self.visited_urls = set()
+        self.transformer = BeautifulSoupTransformer()
 
-    def parse(self, response):
+    def parse(self, response: Response, **kwargs):
         if response.url in self.visited_urls:
             return
         self.visited_urls.add(response.url)
-
+        html_doc = [Document(page_content=response.text)]
+        transformed_doc = self.transformer.transform_documents(html_doc,
+                                                               tags_to_extract=["p", "h1", "h2", "h3", "h4", "h5", "h6",
+                                                                                "a", "li", "ul", "ol", "table", "tr",
+                                                                                "td", "th", "div", "span"])
         # Yield the raw HTML content
         yield {
             "url": response.url,
-            "html": response.text
+            "html": response.text,
+            "page_content": transformed_doc[0].page_content
         }
 
         base_url = response.url.split("/")[2]
