80bcf190ace0878ed1f16156220b677dd0816043
Mohammad Hakimi <hakimi.mohammad.n@gmail.com>
2024-11-23 13:35:28 +0100

    added qa pairs

--- scrapy_test/scrapy_test/spiders/my_spider.py
+++ scrapy_test/scrapy_test/spiders/my_spider.py
@@ -11,7 +11,7 @@ class WebCrawler(scrapy.Spider):
     start_urls = ["https://www.arol.com/arol-canelli"]
     transformer: BeautifulSoupTransformer
     custom_settings = {
-        "DEPTH_LIMIT": 5,
+        "DEPTH_LIMIT": 3,
         "ROBOTSTXT_OBEY": True,
         "USER_AGENT": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
         "DOWNLOAD_DELAY": 2,
